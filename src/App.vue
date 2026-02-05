284217dde28267c4a2c6f157047e166e5a0d1b94
robertkumar <rob_kumar@outlook.com>
2026-02-04 22:57:03 -0500

    made first axios call, need to refactor into composable for reusability

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+async function getCompactDisc() {
+  const response = await axios.get('http://localhost/api/v1/compact-disc', {
+    headers: {
+      Authorization: 'Bearer 1|VDPl73Hqcfs8GUfL0q7SyTzmpIelxIzP2iroNsKD7ee59d30',
+    },
+  })
+}
+
+onMounted(() => {
+  getCompactDisc()
+})
+</script>
 
 <template>
   <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 </template>
 
 <style scoped></style>
