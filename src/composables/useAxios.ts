0ec07650737b78303515542c05debf000b9a1245
robertkumar <rob_kumar@outlook.com>
2026-02-06 17:09:37 -0500

    WIP: useAxios composable coming together

--- src/composables/useAxios.ts
+++ src/composables/useAxios.ts
@@ -1,6 +1,18 @@
 import axios from 'axios';
 import { ref } from 'vue';
+import { useLoadingStore } from '@/stores/loadingStore.ts'
+import { storeToRefs } from 'pinia'
 
 export function useAxios() {
+  const loadingStore = useLoadingStore();
+  const { isLoading } = storeToRefs(loadingStore);
 
+  async function get(endpoint: string): Promise<object> {
+    const response: Promise<object> = axios.get(`${import.meta.env.VITE_API_URL}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+      },
+    })
+    return { get }
+  }
 }
