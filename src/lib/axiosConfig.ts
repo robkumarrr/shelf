40a8dfaf64f553f3964322addc7d4e0965276642
robertkumar <rob_kumar@outlook.com>
2026-02-15 12:06:42 -0500

    remove token auth for axios instance

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -1,16 +1,9 @@
-import axios, { type InternalAxiosRequestConfig } from 'axios'
+import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   withXSRFToken: true,
-})
-
-axiosInstance.interceptors.request.use(function (
-  config: InternalAxiosRequestConfig,
-): InternalAxiosRequestConfig {
-  config.headers.set('Authorization', `Bearer ${import.meta.env.VITE_API_TOKEN}`)
-  return config
 })
 
 export default axiosInstance
