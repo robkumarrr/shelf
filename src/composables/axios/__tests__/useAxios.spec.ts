cf1e0a3c96baa7df7e825a4102e97f5c7082067f
robertkumar <rob_kumar@outlook.com>
2026-02-16 11:11:00 -0500

    WIP: scaffolding testing

---
+++ src/composables/axios/__tests__/useAxios.spec.ts
@@ -0,0 +1,13 @@
+import { vi, describe, it, expect } from 'vitest'
+
+describe('useAxios composable', () => {
+
+  it.todo('tests axios get returns data when called', () => {
+
+  })
+
+  it.todo('tests axios get handles errors from the request', () => {})
+
+  it.todo('tests axios get throws an exception when an unexpected error occurs', () => {})
+
+})
