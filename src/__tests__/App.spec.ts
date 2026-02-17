069d171ef8d1f67f978673cb1f3b9771c3af75f2
robertkumar <rob_kumar@outlook.com>
2026-02-17 09:25:21 -0500

    fix app mount test

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,9 +1,13 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, it, expect, beforeEach } from 'vitest'
 import { mount } from '@vue/test-utils'
 import App from '../App.vue'
+import { createPinia, setActivePinia } from 'pinia'
 
 describe('App', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia())
+  })
+
   it('mounts renders properly', () => {
     const wrapper = mount(App)
     expect(wrapper.exists()).toBe(true);
