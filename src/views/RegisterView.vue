eb45d4b9c7b81588884f69daf53a11ccf80df282
robertkumar <rob_kumar@outlook.com>
2026-02-23 21:45:20 -0500

    build basic register form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Form } from '@primevue/forms'
+import InputText from 'primevue/inputtext'
+import { Button } from 'primevue'
+function onFormSubmit() {
+
+}
+</script>
 
 <template>
-  <h1>Register Page</h1>
+  <h1>Register</h1>
+
+  <Form
+    @submit="onFormSubmit"
+    class="flex justify-center flex-col gap-4"
+  >
+    <div class="flex flex-col gap-1">
+      <InputText name="username" type="text" placeholder="Username" />
+    </div>
+    <div class="flex flex-col gap-1">
+      <InputText name="email" type="text" placeholder="Email" />
+    </div>
+    <Button type="submit" severity="secondary" label="Submit" />
+  </Form>
 </template>
 
 <style scoped></style>
