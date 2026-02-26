600db8d91f5c61934e9bbb686d43aa3077feefc4
robertkumar <rob_kumar@outlook.com>
2026-02-25 21:16:20 -0500

    WIP: started zod validation

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import { Form } from '@primevue/forms'
+import { zodResolver } from '@primevue/forms/resolvers/zod'
+import { z } from 'zod'
 import InputText from 'primevue/inputtext'
 import { Button } from 'primevue'
 import { useToast } from 'primevue/usetoast'
 import Toast from 'primevue/toast'
+import { ref } from 'vue'
 
 const toast = useToast()
-//
+
+const registrationForm = z.object({
+  username: z.string().trim().lowercase().min(5, { message: 'Username is required.' }),
+  email: z.email({ message: 'Email is required.' }).trim(),
+  password: z.string().min(8, { message: 'Password is not long enough' }),
+  password_confirmation: z.string({
+    error: 'Custom error that password confirmation does not match password',
+  }),
+}).refine((regForm) => regForm.password === regForm.password_confirmation, {
+
+})
+
+const resolver = ref(zodResolver(registrationForm))
+
 function onFormSubmit() {
   console.log('Form submitted')
   toast.add({ severity: 'success', summary: 'Registering...', life: 3000 })
@@ -17,12 +33,15 @@ function onFormSubmit() {
   <section class="flex min-h-screen justify-center items-center">
     <Toast />
     <Form
+      v-slot="$form"
+      :resolver
       @submit="onFormSubmit"
       class="flex w-full bg-neutral-50 rounded-2xl drop-shadow-2xl shadow-neutral-300 max-w-lg mx-auto flex-col gap-6 p-20"
     >
       <h1 class="font-bold text-2xl text-center">Register</h1>
       <p class="text-center">
-        Already have an account? Head to the <router-link to="/login">Login</router-link> page.
+        Already have an account? Head to the
+        <router-link class="text-indigo-600" to="/login">Login</router-link> page.
       </p>
       <div class="flex flex-col gap-y-4">
         <InputText name="username" type="text" placeholder="Username" />
