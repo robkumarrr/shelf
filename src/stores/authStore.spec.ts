e900826aefc0fab75a4b93b9078f2b7ed9b03e29
robertkumar <rob_kumar@outlook.com>
2026-03-01 16:49:49 -0500

    add tests for authStore

--- src/stores/authStore.spec.ts
+++ src/stores/authStore.spec.ts
@@ -1,19 +1,33 @@
 import { createPinia, setActivePinia } from 'pinia'
 import { useAuthStore} from '@/stores/authStore.ts'
 import { describe, expect, it, beforeEach, vi } from 'vitest'
+import { ref } from 'vue'
+
+const mockUserStore = {
+  username: ref<string | null>(null),
+  email: ref<string | null>(null),
+}
+
+vi.mock('@/stores/userStore', () => ({
+  useUserStore: () => mockUserStore
+}))
+
+const mockPost = vi.fn().mockResolvedValue({
+  status: 201,
+  data: {
+    attributes: {
+      name: 'testuser',
+      email: 'test@example.com',
+    },
+  },
+})
+
+const mockGet = vi.fn().mockResolvedValue({ data: null })
 
 vi.mock('@/composables/axios/useAxios', () => ({
   useAxios: () => ({
-    get: vi.fn().mockResolvedValue({ data: null }),
-    post: vi.fn().mockResolvedValue({
-      status: 201,
-      data: {
-        attributes: {
-          name: 'testuser',
-          email: 'test@example.com',
-        },
-      },
-    }),
+    get: mockGet,
+    post: mockPost,
   }),
 }))
 
@@ -23,6 +37,8 @@ describe('Auth Store', () => {
 
   beforeEach(() => {
     setActivePinia(createPinia())
+    mockUserStore.email.value = null
+    mockUserStore.username.value = null
     authStore = useAuthStore()
   })
 
@@ -31,8 +47,41 @@ describe('Auth Store', () => {
   })
 
   describe('Register', () => {
-    it('Succesfully registers a user', () => {
+    it('register method makes call to register endpoint', async () => {
+      expect(mockUserStore.email.value).toBe(null)
+      expect(mockUserStore.username.value).toBe(null)
+      expect(authStore.isAuthenticated).toBe(false)
 
+      await authStore.register({
+        name: 'testuser',
+        email: 'test@example.com',
+        password: 'password',
+      })
+
+      expect(mockGet).toBeCalledWith('/sanctum/csrf-cookie')
+      expect(mockGet).toHaveBeenCalledBefore(mockPost)
+
+      expect(authStore.isAuthenticated).toBe(true)
+      expect(mockUserStore.email.value).toBe('test@example.com')
+      expect(mockUserStore.username.value).toBe('testuser')
+    })
+
+    it('register method catches error', async () => {
+      mockPost.mockRejectedValueOnce(new Error('Server error'))
+      expect(authStore.isAuthenticated).toBe(false)
+
+      await expect(authStore.register({
+        name: 'testuser',
+        email: 'test@example.com',
+        password: 'password',
+      })).rejects.toThrow()
+
+      expect(mockGet).toBeCalledWith('/sanctum/csrf-cookie')
+      expect(mockGet).toHaveBeenCalledBefore(mockPost)
+
+      expect(authStore.isAuthenticated).toBe(false)
+      expect(mockUserStore.email.value).toBe(null)
+      expect(mockUserStore.username.value).toBe(null)
     })
   })
 })
